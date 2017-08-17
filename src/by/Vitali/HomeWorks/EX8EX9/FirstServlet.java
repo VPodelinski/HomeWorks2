75d6d5adb3542f969fabacf4a01fb0a34820c8e7
Vitali Podelinski <viivpo2014@gmail.com>
2017-08-17 09:43:59 +0300

    corrected class

--- src/by/Vitali/HomeWorks/EX8EX9/FirstServlet.java
+++ src/by/Vitali/HomeWorks/EX8EX9/FirstServlet.java
@@ -11,12 +11,29 @@ import java.io.*;
  */
 public class FirstServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private int counterVisit = 0;
+    private int counterVisit;
+    // use FileInputStream and FileOutputStream because one wariable is written
+    private FileInputStream input;
+    private FileOutputStream output;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            //path file to store the variable
+            input = new FileInputStream("/opt/tomcat/webapps/examples/WEB-INF/classes/by/Vitali/HomeWorks/EX8EX9/resource/count.txt");
+            try {
+                counterVisit = Integer.parseInt(String.valueOf(input.read()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
-    {
-        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream("/opt/tomcat/webapps/examples/WEB-INF/classes/by/Vitali/HomeWorks/EX8EX9/resource/count.txt"));
+            throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         counterVisit++;
         out.println("<html><head><title>Fist Servlet</title></head>");
@@ -25,11 +42,24 @@ public class FirstServlet extends HttpServlet {
         out.println("<h2>Count of visits " + counterVisit + "</h2>");
         out.println("<hr align=\"left\" width=\"170\" size=\"3\"color=\"#36E052\"/>");
         out.println("</body></html>");
-        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream("/opt/tomcat/webapps/examples/WEB-INF/classes/by/Vitali/HomeWorks/EX8EX9/resource/count.txt"));
+        output = new FileOutputStream("/opt/tomcat/webapps/examples/WEB-INF/classes/by/Vitali/HomeWorks/EX8EX9/resource/count.txt");
+        output.write(counterVisit);
+        output.flush();
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
         doGet(request ,response);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            output.close();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
